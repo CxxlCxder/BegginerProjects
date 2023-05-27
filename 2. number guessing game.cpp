540372aa79646bd6a80cbb40e010a430bd84b66b
CxxlCxder <85243775+CxxlCxder@users.noreply.github.com>
2023-05-27 09:29:47 +0200

    Update 2. number guessing game.cpp
    Removing some of the Todo comments I made

--- 2. number guessing game.cpp
+++ 2. number guessing game.cpp
@@ -8,7 +8,6 @@ using namespace std;
 void banner() {
     cout << "Number Guessing Game Ver 1.0" << endl << endl;
     cout << "[ info ] You have a maximum of 20 tries before losing the game" << endl << "Begin" << endl<<endl;
-    /*cout << "[ 1 ] Start" << endl << "[ 2 ] About" << endl << endl;*/
 }
 
 int main() {
@@ -42,11 +41,3 @@ int main() {
     cout << "You Won" << endl;
     cout << "Amount of tries: " << tries << endl;
 }
-
-/*
-[ 1 ] start 
-
-[ 1 ] easy ( 100 tries and 100 as max limit)
-[ 2 ] moderate ( max is 1K and tries are 20 max )
-[ 3 ] hard ( number can go up to 100K and only 30 tries )
-*/
